5d69e15299b396760523b4f190082de7d9b9a599
valderosch <phonelix96@gmail.com>
2023-01-27 23:19:00 +0200

    initialized

---
+++ src/main/java/com/idea/bakflow/BakflowApplication.java
@@ -0,0 +1,13 @@
+package com.idea.bakflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idea/bakflow/BakflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea.bakflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
