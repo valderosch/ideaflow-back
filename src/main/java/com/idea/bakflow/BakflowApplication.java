8a393fca368dccd381d02a99e7e17d1cfdfddd31
valderosch <phonelix96@gmail.com>
2023-01-29 17:04:02 +0200

    cleanedapp

--- src/main/java/com/idea/bakflow/BakflowApplication.java
+++ src/main/java/com/idea/bakflow/BakflowApplication.java
@@ -17,18 +17,6 @@ public class BakflowApplication {
 	@Bean
 	CommandLineRunner loadData(PostRepository postRepository){
 		return (args) -> {
-			Post post1 = new Post();
-			post1.setAuthor("Vlad");
-			post1.setTitle("Very cool course");
-			post1.setDescription("This is how works java");
-
-			Post post2 = new Post();
-			post2.setAuthor("Gabriel");
-			post2.setTitle("Very cool video about Dogs");
-			post2.setDescription("I love Dogs. This is very funny and friendly animals");
-
-			postRepository.save(post1);
-			postRepository.save(post2);
 		};
 	}
 }
